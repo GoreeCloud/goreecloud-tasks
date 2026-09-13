15fe300dcc275cd94040c665cca2401525ee82a5
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-13 09:18:38 -0500

    Route native client task detail API

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path
 
 from .calendar_views import calendar_task_projections
-from .client_views import client_tasks
+from .client_views import client_task_detail, client_tasks
 from .views import manager_operational_tasks
 
 app_name = "api"
@@ -23,5 +23,10 @@ urlpatterns = [
         "client/tasks/",
         client_tasks,
         name="client-tasks",
+    ),
+    path(
+        "client/tasks/<int:task_id>/",
+        client_task_detail,
+        name="client-task-detail",
     ),
 ]
